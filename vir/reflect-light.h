e285e68f79ce64b2bef42762cde5426858b7973c
Matthias Kretz <m.kretz@gsi.de>
2024-09-06 21:45:48 +0200

    Convert to nicer non-member interface
    ChangeLog:
    
    	* vir/reflect-light.h: Add vir::refl namespace with new
    	functions: data_member and variable templates data_member_count
    	and data_member_name.

--- vir/reflect-light.h
+++ vir/reflect-light.h
@@ -9,54 +9,66 @@
 #include "fixed_string.h"
 #include <tuple>
 
-namespace vir
-{
 #define VIR_PARENS ()
 #define VIR_EXPAND(...) VIR_EXPAND4(VIR_EXPAND4(VIR_EXPAND4(VIR_EXPAND4(__VA_ARGS__))))
 #define VIR_EXPAND4(...) VIR_EXPAND3(VIR_EXPAND3(VIR_EXPAND3(VIR_EXPAND3(__VA_ARGS__))))
 #define VIR_EXPAND3(...) VIR_EXPAND2(VIR_EXPAND2(VIR_EXPAND2(VIR_EXPAND2(__VA_ARGS__))))
 #define VIR_EXPAND2(...) VIR_EXPAND1(VIR_EXPAND1(VIR_EXPAND1(VIR_EXPAND1(__VA_ARGS__))))
 #define VIR_EXPAND1(...) __VA_ARGS__
+
 #define VIR_TO_STRINGS(...) __VA_OPT__(VIR_EXPAND(VIR_TO_STRINGS_IMPL(__VA_ARGS__)))
 #define VIR_TO_STRINGS_IMPL(x, ...) \
   fixed_string(#x) __VA_OPT__(, VIR_TO_STRINGS_AGAIN VIR_PARENS(__VA_ARGS__))
 #define VIR_TO_STRINGS_AGAIN() VIR_TO_STRINGS_IMPL
 
+#define VIR_COUNT_ARGS(...) __VA_OPT__(VIR_EXPAND(VIR_COUNT_ARGS_IMPL(__VA_ARGS__)))
+#define VIR_COUNT_ARGS_IMPL(x, ...) 1 __VA_OPT__(+ VIR_COUNT_ARGS_AGAIN VIR_PARENS(__VA_ARGS__))
+#define VIR_COUNT_ARGS_AGAIN() VIR_COUNT_ARGS_IMPL
+
 #define VIR_MAKE_REFLECTABLE(...)                                                                  \
  public:                                                                                           \
   constexpr decltype(auto)                                                                         \
-  refl_data_member(auto idx) &                                                                     \
-  {                                                                                                \
-    if constexpr (std::convertible_to<decltype(idx), std::size_t>)                                 \
-      return std::get<idx.value>(std::tie(__VA_ARGS__));                                           \
-    else                                                                                           \
-      return [&]<std::size_t... Is>(std::index_sequence<Is...>) -> decltype(auto) {                \
-        return std::get<(((idx == std::get<Is>(refl_data_member_names)) * Is) + ...)>(             \
-                 std::tie(__VA_ARGS__));                                                           \
-      }(std::make_index_sequence<refl_data_member_count>());                                       \
-  }                                                                                                \
+  vir_refl_members_as_tuple() &                                                                    \
+  { return std::tie(__VA_ARGS__); }                                                                \
                                                                                                    \
   constexpr decltype(auto)                                                                         \
-  refl_data_member(auto idx) const&                                                                \
-  {                                                                                                \
-    if constexpr (std::convertible_to<decltype(idx), std::size_t>)                                 \
-      return std::get<idx.value>(std::tie(__VA_ARGS__));                                           \
-    else                                                                                           \
-      return [&]<std::size_t... Is>(std::index_sequence<Is...>) -> decltype(auto) {                \
-        return std::get<(((idx == std::get<Is>(refl_data_member_names)) * Is) + ...)>(             \
-                 std::tie(__VA_ARGS__));                                                           \
-      }(std::make_index_sequence<refl_data_member_count>());                                       \
-  }                                                                                                \
+  vir_refl_members_as_tuple() const&                                                               \
+  { return std::tie(__VA_ARGS__); }                                                                \
                                                                                                    \
-  static constexpr                                                                                 \
-  std::integral_constant<std::size_t, std::tuple_size_v<decltype(std::make_tuple(__VA_ARGS__))>>   \
-    refl_data_member_count {};                                                                     \
+  static constexpr std::integral_constant<std::size_t, VIR_COUNT_ARGS(__VA_ARGS__)>                \
+    vir_refl_data_member_count {};                                                                 \
                                                                                                    \
-  static constexpr auto refl_data_member_names = std::tuple{VIR_TO_STRINGS(__VA_ARGS__)}
+  static constexpr auto vir_refl_data_member_names = std::tuple{VIR_TO_STRINGS(__VA_ARGS__)}
+
+namespace vir
+{
+  namespace refl
+  {
+    template <size_t Idx>
+      constexpr decltype(auto)
+      data_member(auto&& obj)
+      { return std::get<Idx>(obj.vir_refl_members_as_tuple()); }
+
+    template <fixed_string Name>
+      constexpr decltype(auto)
+      data_member(auto&& obj)
+      {
+        using Class = std::remove_cvref_t<decltype(obj)>;
+        return [&]<std::size_t... Is>(std::index_sequence<Is...>) -> decltype(auto) {
+          return std::get<(((Name == std::get<Is>(Class::vir_refl_data_member_names)) * Is) + ...)>(
+                   obj.vir_refl_members_as_tuple());
+        }(std::make_index_sequence<Class::vir_refl_data_member_count>());
+      }
+
+    template <typename T>
+      constexpr auto data_member_count = T::vir_refl_data_member_count;
+
+    template <typename T, size_t Idx>
+      constexpr auto data_member_name = std::get<Idx>(T::vir_refl_data_member_names);
+  }
 
   namespace _test_
   {
-    using namespace vir::literals;
     struct Test
     {
       int a, b, foo;
@@ -64,33 +76,29 @@ namespace vir
       VIR_MAKE_REFLECTABLE(a, b, foo);
     };
 
-    template <auto X>
-      static constexpr std::integral_constant<std::remove_const_t<decltype(X)>, X>
-      ic = {};
-
     static_assert([] {
       Test t {1, 2, 3};
-      if (t.refl_data_member_count != 3)
+      if (refl::data_member_count<Test>  != 3)
         return false;
-      if (std::get<0>(t.refl_data_member_names) != "a")
+      if (refl::data_member_name<Test, 0> != "a")
         return false;
-      if (std::get<1>(t.refl_data_member_names) != "b")
+      if (refl::data_member_name<Test, 1> != "b")
         return false;
-      if (std::get<2>(t.refl_data_member_names) != "foo")
+      if (refl::data_member_name<Test, 2> != "foo")
         return false;
-      if (&t.refl_data_member(ic<0>) != &t.a)
+      if (&refl::data_member<0>(t) != &t.a)
         return false;
-      if (&t.refl_data_member(ic<1>) != &t.b)
+      if (&refl::data_member<1>(t) != &t.b)
         return false;
-      if (&t.refl_data_member(ic<2>) != &t.foo)
+      if (&refl::data_member<2>(t) != &t.foo)
         return false;
-      if (&t.refl_data_member("a"_ic) != &t.a)
+      if (&refl::data_member<"a">(t) != &t.a)
         return false;
-      if (&t.refl_data_member("b"_ic) != &t.b)
+      if (&refl::data_member<"b">(t) != &t.b)
         return false;
-      if (&t.refl_data_member("foo"_ic) != &t.foo)
+      if (&refl::data_member<"foo">(t) != &t.foo)
         return false;
-      t.refl_data_member(ic<1>) = -1;
+      refl::data_member<"b">(t) = -1;
       if (t.b != -1)
         return false;
       return true;
