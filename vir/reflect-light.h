b7bc04e918f2ea1b05b72581e489d6b859704293
Matthias Kretz <m.kretz@gsi.de>
2024-09-09 09:46:11 +0200

    Improve macro namespacing (name prefix)
    ChangeLog:
    
    	* vir/reflect-light.h: Rename internal macros.

--- vir/reflect-light.h
+++ vir/reflect-light.h
@@ -9,21 +9,31 @@
 #include "fixed_string.h"
 #include <tuple>
 
-#define VIR_PARENS ()
-#define VIR_EXPAND(...) VIR_EXPAND4(VIR_EXPAND4(VIR_EXPAND4(VIR_EXPAND4(__VA_ARGS__))))
-#define VIR_EXPAND4(...) VIR_EXPAND3(VIR_EXPAND3(VIR_EXPAND3(VIR_EXPAND3(__VA_ARGS__))))
-#define VIR_EXPAND3(...) VIR_EXPAND2(VIR_EXPAND2(VIR_EXPAND2(VIR_EXPAND2(__VA_ARGS__))))
-#define VIR_EXPAND2(...) VIR_EXPAND1(VIR_EXPAND1(VIR_EXPAND1(VIR_EXPAND1(__VA_ARGS__))))
-#define VIR_EXPAND1(...) __VA_ARGS__
+// recursive macro implementation inspired by https://www.scs.stanford.edu/~dm/blog/va-opt.html
 
-#define VIR_TO_STRINGS(...) __VA_OPT__(VIR_EXPAND(VIR_TO_STRINGS_IMPL(__VA_ARGS__)))
-#define VIR_TO_STRINGS_IMPL(x, ...) \
-  ::vir::fixed_string(#x) __VA_OPT__(, VIR_TO_STRINGS_AGAIN VIR_PARENS(__VA_ARGS__))
-#define VIR_TO_STRINGS_AGAIN() VIR_TO_STRINGS_IMPL
+#define VIR_REFLECT_LIGHT_PARENS ()
+#define VIR_REFLECT_LIGHT_EXPAND(...) VIR_REFLECT_LIGHT_EXPAND3(VIR_REFLECT_LIGHT_EXPAND3(         \
+                                        VIR_REFLECT_LIGHT_EXPAND3(VIR_REFLECT_LIGHT_EXPAND3(       \
+                                          __VA_ARGS__))))
+#define VIR_REFLECT_LIGHT_EXPAND3(...) VIR_REFLECT_LIGHT_EXPAND2(VIR_REFLECT_LIGHT_EXPAND2(        \
+                                         VIR_REFLECT_LIGHT_EXPAND2(VIR_REFLECT_LIGHT_EXPAND2(      \
+                                           __VA_ARGS__))))
+#define VIR_REFLECT_LIGHT_EXPAND2(...) VIR_REFLECT_LIGHT_EXPAND1(VIR_REFLECT_LIGHT_EXPAND1(        \
+                                         VIR_REFLECT_LIGHT_EXPAND1(VIR_REFLECT_LIGHT_EXPAND1(      \
+                                           __VA_ARGS__))))
+#define VIR_REFLECT_LIGHT_EXPAND1(...) __VA_ARGS__
 
-#define VIR_COUNT_ARGS(...) __VA_OPT__(VIR_EXPAND(VIR_COUNT_ARGS_IMPL(__VA_ARGS__)))
-#define VIR_COUNT_ARGS_IMPL(x, ...) 1 __VA_OPT__(+ VIR_COUNT_ARGS_AGAIN VIR_PARENS(__VA_ARGS__))
-#define VIR_COUNT_ARGS_AGAIN() VIR_COUNT_ARGS_IMPL
+#define VIR_REFLECT_LIGHT_TO_STRINGS(...)                                                          \
+  __VA_OPT__(VIR_REFLECT_LIGHT_EXPAND(VIR_REFLECT_LIGHT_TO_STRINGS_IMPL(__VA_ARGS__)))
+#define VIR_REFLECT_LIGHT_TO_STRINGS_IMPL(x, ...) ::vir::fixed_string(#x)                          \
+  __VA_OPT__(, VIR_REFLECT_LIGHT_TO_STRINGS_AGAIN VIR_REFLECT_LIGHT_PARENS(__VA_ARGS__))
+#define VIR_REFLECT_LIGHT_TO_STRINGS_AGAIN() VIR_REFLECT_LIGHT_TO_STRINGS_IMPL
+
+#define VIR_REFLECT_LIGHT_COUNT_ARGS(...)                                                          \
+  __VA_OPT__(VIR_REFLECT_LIGHT_EXPAND(VIR_REFLECT_LIGHT_COUNT_ARGS_IMPL(__VA_ARGS__)))
+#define VIR_REFLECT_LIGHT_COUNT_ARGS_IMPL(x, ...) 1                                                \
+  __VA_OPT__(+ VIR_REFLECT_LIGHT_COUNT_ARGS_AGAIN VIR_REFLECT_LIGHT_PARENS(__VA_ARGS__))
+#define VIR_REFLECT_LIGHT_COUNT_ARGS_AGAIN() VIR_REFLECT_LIGHT_COUNT_ARGS_IMPL
 
 #define VIR_MAKE_REFLECTABLE(...)                                                                  \
  public:                                                                                           \
@@ -35,10 +45,11 @@
   vir_refl_members_as_tuple() const&                                                               \
   { return std::tie(__VA_ARGS__); }                                                                \
                                                                                                    \
-  static constexpr std::integral_constant<std::size_t, VIR_COUNT_ARGS(__VA_ARGS__)>                \
+  static constexpr std::integral_constant<std::size_t, VIR_REFLECT_LIGHT_COUNT_ARGS(__VA_ARGS__)>  \
     vir_refl_data_member_count {};                                                                 \
                                                                                                    \
-  static constexpr auto vir_refl_data_member_names = std::tuple{VIR_TO_STRINGS(__VA_ARGS__)}
+  static constexpr auto vir_refl_data_member_names                                                 \
+    = std::tuple{VIR_REFLECT_LIGHT_TO_STRINGS(__VA_ARGS__)}
 
 namespace vir
 {
