77c8eea26a560d71a4416d7a2f61547d4e511b3e
Matthias Kretz <m.kretz@gsi.de>
2024-09-13 21:51:28 +0200

    Reduce base classes of simple_tuple and hide internals better
    ChangeLog:
    
    	* vir/simple_tuple.h: Rename get to get_impl_. Rename
    	type_at_impl to type_at_impl_. Fully implement tuple_data with
    	1, 2, 3, and 4 values. Reducing the number of base classes for
    	large tuples by a factor of 4.

--- vir/simple_tuple.h
+++ vir/simple_tuple.h
@@ -15,88 +15,97 @@ namespace vir
     template <auto X>
       inline constexpr std::integral_constant<std::remove_const_t<decltype(X)>, X> ic {};
 
-#define VIR_TUPLE_DATA_COMPARES                                \
+#define VIR_SIMPLE_TUPLE_DATA_COMPARES                         \
   friend constexpr bool                                        \
   operator==(tuple_data const&, tuple_data const&) = default;  \
                                                                \
   friend constexpr auto                                        \
   operator<=>(tuple_data const&, tuple_data const&) = default
 
+#define VIR_SIMPLE_TUPLE_DATA_IMPL(i)                          \
+  T##i value##i;                                               \
+                                                               \
+  template <typename Idx>                                      \
+    requires (Idx::value == Off + i)                           \
+    friend constexpr T##i&&                                    \
+    get_impl_(tuple_data&& obj, Idx) noexcept                  \
+    { return static_cast<T##i&&>(obj.value##i); }              \
+                                                               \
+  template <typename Idx>                                      \
+    requires (Idx::value == Off + i)                           \
+    friend constexpr T##i&                                     \
+    get_impl_(tuple_data& obj, Idx) noexcept                   \
+    { return obj.value##i; }                                   \
+                                                               \
+  template <typename Idx>                                      \
+    requires (Idx::value == Off + i)                           \
+    friend constexpr T##i const&                               \
+    get_impl_(tuple_data const& obj, Idx) noexcept             \
+    { return obj.value##i; }                                   \
+                                                               \
+  template <typename Idx>                                      \
+    requires (Idx::value == Off + i)                           \
+    friend consteval T##i                                      \
+    type_at_impl_(tuple_data const&, Idx)                      \
+    { return std::declval<T##i>(); }
+
     template <int Off, typename... Ts>
       struct tuple_data;
 
     template <>
       struct tuple_data<0>
-      { VIR_TUPLE_DATA_COMPARES; };
+      { VIR_SIMPLE_TUPLE_DATA_COMPARES; };
 
     template <int Off, typename T0>
       struct tuple_data<Off, T0>
       {
-        T0 value;
-
-        template <typename Idx>
-          requires (Idx::value == Off)
-          friend constexpr T0&&
-          get(tuple_data&& obj, Idx) noexcept
-          { return static_cast<T0&&>(obj.value); }
-
-        template <typename Idx>
-          requires (Idx::value == Off)
-          friend constexpr T0&
-          get(tuple_data& obj, Idx) noexcept
-          { return obj.value; }
-
-        template <typename Idx>
-          requires (Idx::value == Off)
-          friend constexpr T0 const&
-          get(tuple_data const& obj, Idx) noexcept
-          { return obj.value; }
-
-        template <typename Idx>
-          requires (Idx::value == Off)
-          friend consteval T0
-          type_at_impl(tuple_data const&, Idx)
-          { return std::declval<T0>(); }
-
-        VIR_TUPLE_DATA_COMPARES;
+        VIR_SIMPLE_TUPLE_DATA_IMPL(0)
+        VIR_SIMPLE_TUPLE_DATA_COMPARES;
       };
 
     template <int Off, typename T0, typename T1>
       struct tuple_data<Off, T0, T1>
-      : tuple_data<Off, T0>, tuple_data<Off + 1, T1>
-      { VIR_TUPLE_DATA_COMPARES; };
+      {
+        VIR_SIMPLE_TUPLE_DATA_IMPL(0)
+        VIR_SIMPLE_TUPLE_DATA_IMPL(1)
+        VIR_SIMPLE_TUPLE_DATA_COMPARES;
+      };
 
     template <int Off, typename T0, typename T1, typename T2>
       struct tuple_data<Off, T0, T1, T2>
-      : tuple_data<Off, T0>, tuple_data<Off + 1, T1>, tuple_data<Off + 2, T2>
-      { VIR_TUPLE_DATA_COMPARES; };
+      {
+        VIR_SIMPLE_TUPLE_DATA_IMPL(0)
+        VIR_SIMPLE_TUPLE_DATA_IMPL(1)
+        VIR_SIMPLE_TUPLE_DATA_IMPL(2)
+        VIR_SIMPLE_TUPLE_DATA_COMPARES;
+      };
 
     template <int Off, typename T0, typename T1, typename T2, typename T3>
       struct tuple_data <Off, T0, T1, T2, T3>
-      : tuple_data<Off, T0>, tuple_data<Off + 1, T1>,
-        tuple_data<Off + 2, T2>, tuple_data<Off + 3, T3>
-      { VIR_TUPLE_DATA_COMPARES; };
+      {
+        VIR_SIMPLE_TUPLE_DATA_IMPL(0)
+        VIR_SIMPLE_TUPLE_DATA_IMPL(1)
+        VIR_SIMPLE_TUPLE_DATA_IMPL(2)
+        VIR_SIMPLE_TUPLE_DATA_IMPL(3)
+        VIR_SIMPLE_TUPLE_DATA_COMPARES;
+      };
 
     template <int Off, typename T0, typename T1, typename T2, typename T3, typename... More>
       requires (sizeof...(More) > 0) and (sizeof...(More) <= 4)
       struct tuple_data<Off, T0, T1, T2, T3, More...>
-      : tuple_data<Off, T0>, tuple_data<Off + 1, T1>,
-        tuple_data<Off + 2, T2>, tuple_data<Off + 3, T3>,
-        tuple_data<Off + 4, More...>
-      { VIR_TUPLE_DATA_COMPARES; };
+      : tuple_data<Off, T0, T1, T2, T3>, tuple_data<Off + 4, More...>
+      { VIR_SIMPLE_TUPLE_DATA_COMPARES; };
 
     template <int Off, typename T0, typename T1, typename T2, typename T3,
               typename T4, typename T5, typename T6, typename T7, typename... More>
       requires (sizeof...(More) > 0)
       struct tuple_data<Off, T0, T1, T2, T3, T4, T5, T6, T7, More...>
-      : tuple_data<Off, T0>, tuple_data<Off + 1, T1>,
-        tuple_data<Off + 2, T2>, tuple_data<Off + 3, T3>,
-        tuple_data<Off + 4, T4>, tuple_data<Off + 5, T5>,
-        tuple_data<Off + 6, T6>, tuple_data<Off + 7, T7>,
+      : tuple_data<Off, T0, T1, T2, T3>, tuple_data<Off + 4, T4, T5, T6, T7>,
         tuple_data<Off + 8, More...>
-      { VIR_TUPLE_DATA_COMPARES; };
+      { VIR_SIMPLE_TUPLE_DATA_COMPARES; };
 
-#undef VIR_TUPLE_DATA_COMPARES
+#undef VIR_SIMPLE_TUPLE_DATA_IMPL
+#undef VIR_SIMPLE_TUPLE_DATA_COMPARES
   }
 
   template <typename... Ts>
@@ -119,7 +128,7 @@ namespace vir
         operator[](Idx i) && noexcept
         {
           static_assert(Idx::value >= 0 and Idx::value < sizeof...(Ts));
-          return get(static_cast<simple_tuple&&>(*this), i);
+          return get_impl_(static_cast<simple_tuple&&>(*this), i);
         }
 
       template <typename Idx>
@@ -127,7 +136,7 @@ namespace vir
         operator[](Idx i) & noexcept
         {
           static_assert(Idx::value >= 0 and Idx::value < sizeof...(Ts));
-          return get(*this, i);
+          return get_impl_(*this, i);
         }
 
       template <typename Idx>
@@ -135,13 +144,13 @@ namespace vir
         operator[](Idx i) const& noexcept
         {
           static_assert(Idx::value >= 0 and Idx::value < sizeof...(Ts));
-          return get(*this, i);
+          return get_impl_(*this, i);
         }
 
       template <size_t Idx>
         requires (Idx < sizeof...(Ts))
-        using type_at = decltype(type_at_impl(std::declval<detail::tuple_data<0, Ts...>>(),
-                                              detail::ic<Idx>));
+        using type_at = decltype(type_at_impl_(std::declval<detail::tuple_data<0, Ts...>>(),
+                                               detail::ic<Idx>));
 
       friend constexpr bool
       operator==(simple_tuple const&, simple_tuple const&) = default;
@@ -157,7 +166,7 @@ namespace vir
           return [&]<size_t... Is, size_t... Js>(std::index_sequence<Is...>,
                                                  std::index_sequence<Js...>) {
             return simple_tuple<Ts..., Us...> {
-              get(lhs, detail::ic<Is>)..., get(rhs, detail::ic<Js>)...
+              get_impl_(lhs, detail::ic<Is>)..., get_impl_(rhs, detail::ic<Js>)...
             };
           }(lhs.size_sequence, rhs.size_sequence);
         }
@@ -169,7 +178,7 @@ namespace vir
           return [&]<size_t... Is, size_t... Js>(std::index_sequence<Is...>,
                                                  std::index_sequence<Js...>) {
             return simple_tuple<Ts..., Us...> {
-              get(lhs, detail::ic<Is>)..., get(rhs, detail::ic<Js>)...
+              get_impl_(lhs, detail::ic<Is>)..., get_impl_(rhs, detail::ic<Js>)...
             };
           }(lhs.size_sequence, rhs.size_sequence);
         }
@@ -179,7 +188,7 @@ namespace vir
       for_each(auto&& fun)
       {
         [&]<size_t... Is>(std::index_sequence<Is...>) {
-          (fun(get(*this, detail::ic<Is>)), ...);
+          (fun(get_impl_(*this, detail::ic<Is>)), ...);
         }(size_sequence);
       }
 
@@ -187,7 +196,7 @@ namespace vir
       for_each(auto&& fun) const
       {
         [&]<size_t... Is>(std::index_sequence<Is...>) {
-          (fun(get(*this, detail::ic<Is>)), ...);
+          (fun(get_impl_(*this, detail::ic<Is>)), ...);
         }(size_sequence);
       }
 
@@ -195,7 +204,7 @@ namespace vir
       for_all(auto&& fun)
       {
         return [&]<size_t... Is>(std::index_sequence<Is...>) {
-          return fun(get(*this, detail::ic<Is>)...);
+          return fun(get_impl_(*this, detail::ic<Is>)...);
         }(size_sequence);
       }
 
@@ -203,7 +212,7 @@ namespace vir
       for_all(auto&& fun) const
       {
         return [&]<size_t... Is>(std::index_sequence<Is...>) {
-          return fun(get(*this, detail::ic<Is>)...);
+          return fun(get_impl_(*this, detail::ic<Is>)...);
         }(size_sequence);
       }
 
@@ -211,8 +220,9 @@ namespace vir
       transform(auto&& fun) const
       {
         return [&]<size_t... Is>(std::index_sequence<Is...>) {
-          return simple_tuple<std::remove_cvref_t<decltype(fun(get(*this, detail::ic<Is>)))>...> {
-            fun(get(*this, detail::ic<Is>))...
+          return simple_tuple<std::remove_cvref_t<
+                                decltype(fun(get_impl_(*this, detail::ic<Is>)))>...> {
+            fun(get_impl_(*this, detail::ic<Is>))...
           };
         }(size_sequence);
       }
